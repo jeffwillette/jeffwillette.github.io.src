8fd04f926cad7fade96e5151c7ef743803f0aaff
Jeff Willette <jrwillette88@gmail.com>
2018-12-09 20:27:33 +0900

    Added basic global layout
    - removed typography plugin in Favor of mui typography
    - added drawer and basic layout for all pages
    - added an A component to use for links
    - added Ubuntu font from cdn to html.tsx file

--- src/gatsby-queries.d.ts
+++ src/gatsby-queries.d.ts
@@ -2,6 +2,28 @@
 // This file was automatically generated and should not be edited.
 
 // ====================================================
+// GraphQL query operation: HeaderQuery
+// ====================================================
+
+export interface HeaderQuery_site_siteMetadata {
+  __typename: "siteMetadata_2";
+  twitter: string | null;
+  github: string | null;
+}
+
+export interface HeaderQuery_site {
+  __typename: "Site";
+  siteMetadata: HeaderQuery_site_siteMetadata | null;
+}
+
+export interface HeaderQuery {
+  site: HeaderQuery_site | null;
+}
+
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
 // GraphQL query operation: IndexQuery
 // ====================================================
 
@@ -18,37 +40,8 @@ export interface IndexQuery_site {
   siteMetadata: IndexQuery_site_siteMetadata | null;
 }
 
-export interface IndexQuery_allFile_edges_node {
-  __typename: "File";
-  /**
-   * The id of this node.
-   */
-  id: string;
-  modifiedTime: any | null;
-}
-
-export interface IndexQuery_allFile_edges {
-  __typename: "FileEdge";
-  /**
-   * The item at the end of the edge
-   */
-  node: IndexQuery_allFile_edges_node | null;
-}
-
-export interface IndexQuery_allFile {
-  __typename: "FileConnection";
-  /**
-   * A list of edges.
-   */
-  edges: (IndexQuery_allFile_edges | null)[] | null;
-}
-
 export interface IndexQuery {
   site: IndexQuery_site | null;
-  /**
-   * Connection to all File nodes
-   */
-  allFile: IndexQuery_allFile | null;
 }
 
 /* tslint:disable */
