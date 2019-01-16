bbd0aa792918b49b7f2fc568187c603f42b52fb8
Jeff Willette <jrwillette88@gmail.com>
2019-01-16 12:58:14 +0900

    Added onClick to tags to navigate to tag page

--- src/components/tagChip.tsx
+++ src/components/tagChip.tsx
@@ -1,4 +1,6 @@
 import { Chip, createStyles, Theme, WithStyles, withStyles } from '@material-ui/core';
+import { navigate } from 'gatsby';
+import GithubSlugger from 'github-slugger';
 import React from 'react';
 
 const styles = (theme: Theme) =>
@@ -12,6 +14,23 @@ interface Props extends WithStyles<typeof styles> {
   tag: string;
 }
 
-const tagChip = ({ classes, tag }: Props) => <Chip clickable variant="outlined" className={classes.chip} label={tag} />;
+const slugger = new GithubSlugger();
+
+const tagChip = ({ classes, tag }: Props) => {
+  // I can make the slug here because this is the same thing that gatsby node does. If this
+  // slugs to the same thing, then it should go to the right place
+  const tagSlug = slugger.slug(tag);
+  slugger.reset();
+
+  return (
+    <Chip
+      clickable
+      onClick={() => navigate(`/tags/${tagSlug}`)}
+      variant="outlined"
+      className={classes.chip}
+      label={tag}
+    />
+  );
+};
 
 export const TagChip = withStyles(styles)(tagChip);
